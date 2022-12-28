047ad195f0c220a4d376cd1ca3d3cc090287da36
flynn81 <tom.karam@gmail.com>
2022-12-28 14:56:19 -0500

    Finishing up day 9 part 1

--- day09/part1/go/main.go
+++ day09/part1/go/main.go
@@ -42,28 +42,69 @@ func abs(i int) int {
 	}
 }
 
+func directlyDown(tc coordinates, hc coordinates) bool {
+	if tc.x == hc.x && tc.y - hc.y == 2 {
+		return true
+	}
+	return false
+}
+
+func directlyUp(tc coordinates, hc coordinates) bool {
+	if tc.x == hc.x && hc.y - tc.y == 2 {
+		return true
+	}
+	return false
+}
+
+func directlyLeft(tc coordinates, hc coordinates) bool {
+	if tc.y == hc.y && tc.x - hc.x == 2 {
+		return true
+	}
+	return false
+}
+
+func directlyRight(tc coordinates, hc coordinates) bool {
+	if tc.y == hc.y && hc.x - tc.x == 2 {
+		return true
+	}
+	return false
+}
+
+func touching(tc coordinates, hc coordinates) bool {
+	if abs(tc.x - hc.x) <= 1 && abs(tc.y - hc.y) <= 1 {
+		return true
+	}
+	return false
+}
+
 func moveTail(tc coordinates, hc coordinates) coordinates {
-	moveHorizontal := true
-	moveVertical := true
-	if abs(tc.x - hc.x)  <= 1 {
-		moveHorizontal = false
-	}
-	if abs(tc.y - hc.y)  <= 1 {
-		moveVertical = false
-	}
-	if moveVertical {
-		if tc.y > hc.y {
-			tc.y--
-		} else {
-			tc.y++
-		}
-	}
-	if moveHorizontal {
-		if tc.x > hc.x {
-			tc.x--
-		} else {
-			tc.x++
-		}
+
+	du := directlyUp(tc,hc)
+	dd := directlyDown(tc,hc)
+	dl := directlyLeft(tc,hc)
+	dr := directlyRight(tc,hc)
+	t := touching(tc,hc)
+	if du {
+		tc.y++
+	} else if dd {
+		tc.y--
+	} else if dl {
+		tc.x--
+	} else if dr {
+		tc.x++
+	} else if tc.y == hc.y && tc.x == hc.x {
+		return tc
+	} else if !t {
+			if tc.x > hc.x {
+				tc.x--
+			} else {
+				tc.x++
+			}
+			if tc.y > hc.y {
+				tc.y --
+			} else {
+				tc.y++
+			}
 	}
 	return tc
 }
@@ -104,14 +145,28 @@ func howManyUniqueVisited(fn string) int {
 	tc := coordinates{0,0}
 	hc := coordinates{0,0}
 	tailVisits := make(map[string]bool)
+	tailVisits[createKey(tc)] = true
 	for fileScanner.Scan() {
 		d, m := processLine(fileScanner.Text())
 		for i:=0; i<m; i++ {
+			fmt.Println("***************************")
 			hc = moveHead(d, hc)
 			fmt.Println(hc)
 			tc = moveTail(tc, hc)
 			fmt.Println(tc)
 			tailVisits[createKey(tc)] = true
+			for y:=10; y>-10; y-- {
+				for x:=-10; x<10; x++ {
+					if x == tc.x && y == tc.y {
+						fmt.Print("T")
+					} else if x == hc.x && y == hc.y {
+						fmt.Print("H")
+					} else {
+						fmt.Print(".")
+					}
+				}
+				fmt.Println("")
+			}
 		}
 	}
 
